76492859bccd29c9a4e93dfa9f40397bd57345a1
andrey <andrey@cossacklabs.com>
2016-09-14 08:37:28 +0300

    Add crypter

--- src/mid_hermes/crypt.c
+++ src/mid_hermes/crypt.c
@@ -57,46 +57,46 @@ bool read_data_from_pipe(int pipe, uint8_t** data, uint32_t* data_length){
 hermes_crypter_t* hermes_crypter_create(const uint8_t* private_key, const size_t private_key_length){
   hermes_crypter_t* crypter = calloc(sizeof(hermes_crypter_t),1);
   HERMES_CHECK(crypter, return NULL);
-  HERMES_CHECK(-1 == pipe(crypter->crypt_reader_pipe), hermes_crypter_destroy(&crypter); return NULL);
-  HERMES_CHECK(-1 == pipe(crypter->crypt_writer_pipe), hermes_crypter_destroy(&crypter); return NULL);
+  HERMES_CHECK(0 == pipe(crypter->crypt_reader_pipe), hermes_crypter_destroy(&crypter); return NULL);
+  HERMES_CHECK(0 == pipe(crypter->crypt_writer_pipe), hermes_crypter_destroy(&crypter); return NULL);
   crypter->crypter_pid = fork();
-  HERMES_CHECK(-1 == crypter->crypter_pid, hermes_crypter_destroy(&crypter); return NULL);
+  HERMES_CHECK(-1 != crypter->crypter_pid, hermes_crypter_destroy(&crypter); return NULL);
   if(0 == crypter->crypter_pid){
     close(crypter->crypt_reader_pipe[1]);
     close(crypter->crypt_writer_pipe[0]);
     uint32_t command=0;
     uint8_t* buf;
     while(true){
-      HERMES_CHECK(sizeof(uint32_t) == read(crypter->crypt_reader_pipe[0], &command, sizeof(uint32_t)), break);
+      ssize_t a=read(crypter->crypt_reader_pipe[0], &command, sizeof(uint32_t));
+      fprintf(stderr, "%i\n", a);
+      HERMES_CHECK(sizeof(uint32_t) == a, _exit(EXIT_FAILURE));
       size_t key_length, data_length, res_data_length;
       uint8_t *key, *data, *res_data;
-      bool error=false;
       switch(command){
       case EXIT:
 	command = EXITED;
-	HERMES_CHECK(sizeof(uint32_t)==write(crypter->crypt_writer_pipe[1], &command, sizeof(uint32_t)), error=true; break);
-	error=true;
-	break;
+	HERMES_CHECK(sizeof(uint32_t)==write(crypter->crypt_writer_pipe[1], &command, sizeof(uint32_t)), _exit(EXIT_FAILURE));
+	_exit(EXIT_SUCCESS);
       case ENCRYPT:
-	HERMES_CHECK(read_data_from_pipe(crypter->crypt_reader_pipe[0], &key, (uint32_t*)(&key_length)), error=true; break);
-	HERMES_CHECK(read_data_from_pipe(crypter->crypt_reader_pipe[0], &data, (uint32_t*)(&data_length)), free(key); error=true; break);
-	HERMES_CHECK(THEMIS_BUFFER_TOO_SMALL == themis_secure_message_wrap(private_key, private_key_length, key, key_length, data, data_length, NULL, &res_data_length), free(key); free(data); error=true; break);
+	HERMES_CHECK(read_data_from_pipe(crypter->crypt_reader_pipe[0], &key, (uint32_t*)(&key_length)), _exit(EXIT_FAILURE));
+	HERMES_CHECK(read_data_from_pipe(crypter->crypt_reader_pipe[0], &data, (uint32_t*)(&data_length)), _exit(EXIT_FAILURE));
+	HERMES_CHECK(THEMIS_BUFFER_TOO_SMALL == themis_secure_message_wrap(private_key, private_key_length, key, key_length, data, data_length, NULL, &res_data_length), _exit(EXIT_FAILURE));
 	res_data=malloc(res_data_length);
-	HERMES_CHECK(res_data && THEMIS_SUCCESS == themis_secure_message_wrap(private_key, private_key_length, key, key_length, data, data_length, res_data, &res_data_length), free(key); free(data); free(res_data); error=true; break);
+	HERMES_CHECK(res_data && THEMIS_SUCCESS == themis_secure_message_wrap(private_key, private_key_length, key, key_length, data, data_length, res_data, &res_data_length), _exit(EXIT_FAILURE));
 	free(key);
 	free(data);
-	HERMES_CHECK(write_data_to_pipe(crypter->crypt_writer_pipe[1], res_data, (uint32_t)res_data_length), free(res_data); error=true; break);
+	HERMES_CHECK(write_data_to_pipe(crypter->crypt_writer_pipe[1], res_data, (uint32_t)res_data_length), _exit(EXIT_FAILURE));
 	free(res_data);
 	break;
       case DECRYPT:
-	HERMES_CHECK(read_data_from_pipe(crypter->crypt_reader_pipe[0], &key, (uint32_t*)(&key_length)), error=true; break);
-	HERMES_CHECK(read_data_from_pipe(crypter->crypt_reader_pipe[0], &data, (uint32_t*)(&data_length)), free(key); error=true; break);
-	HERMES_CHECK(THEMIS_BUFFER_TOO_SMALL == themis_secure_message_unwrap(private_key, private_key_length, key, key_length, data, data_length, NULL, &res_data_length), free(key); free(data); error=true; break);
+	HERMES_CHECK(read_data_from_pipe(crypter->crypt_reader_pipe[0], &key, (uint32_t*)(&key_length)), _exit(EXIT_FAILURE));
+	HERMES_CHECK(read_data_from_pipe(crypter->crypt_reader_pipe[0], &data, (uint32_t*)(&data_length)), _exit(EXIT_FAILURE));
+	HERMES_CHECK(THEMIS_BUFFER_TOO_SMALL == themis_secure_message_unwrap(private_key, private_key_length, key, key_length, data, data_length, NULL, &res_data_length), _exit(EXIT_FAILURE));
 	res_data=malloc(res_data_length);
-	HERMES_CHECK(res_data && THEMIS_SUCCESS == themis_secure_message_unwrap(private_key, private_key_length, key, key_length, data, data_length, res_data, &res_data_length), free(key); free(data); free(res_data); error=true; break);
+	HERMES_CHECK(res_data && THEMIS_SUCCESS == themis_secure_message_unwrap(private_key, private_key_length, key, key_length, data, data_length, res_data, &res_data_length), _exit(EXIT_FAILURE));
 	free(key);
 	free(data);
-	HERMES_CHECK(write_data_to_pipe(crypter->crypt_writer_pipe[1], res_data, (uint32_t)res_data_length), free(res_data); error=true; break);
+	HERMES_CHECK(write_data_to_pipe(crypter->crypt_writer_pipe[1], res_data, (uint32_t)res_data_length), _exit(EXIT_FAILURE));
 	free(res_data);
 	break;
       case SIGN:
@@ -104,13 +104,13 @@ hermes_crypter_t* hermes_crypter_create(const uint8_t* private_key, const size_t
       default:
 	break;
       }
-      HERMES_CHECK(!error, break);
     }
     close(crypter->crypt_reader_pipe[0]);
     close(crypter->crypt_writer_pipe[1]);
     _exit(EXIT_SUCCESS);
   }else{
-
+    close(crypter->crypt_reader_pipe[0]);
+    close(crypter->crypt_writer_pipe[1]);
   }
   return crypter;
 }
@@ -119,8 +119,8 @@ void hermes_crypter_destroy(hermes_crypter_t** crypter){
   uint32_t command = EXIT;
   HERMES_CHECK(sizeof(uint32_t)==write((*crypter)->crypt_reader_pipe[1], &command, sizeof(uint32_t)), return);
   HERMES_CHECK(sizeof(uint32_t) == read((*crypter)->crypt_writer_pipe[0], &command, sizeof(uint32_t)), return);
-  close((*crypter)->crypt_reader_pipe[0]);
-  close((*crypter)->crypt_writer_pipe[1]);
+  close((*crypter)->crypt_reader_pipe[1]);
+  close((*crypter)->crypt_writer_pipe[0]);
   free(*crypter);
   return;
 }
