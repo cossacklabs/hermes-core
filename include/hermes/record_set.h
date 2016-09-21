437b56294400cfb7e3fbefe698fbe5f3d967f749
andrey <andrey@cossacklabs.com>
2016-09-21 18:04:24 +0300

    Add license to all files

--- include/hermes/record_set.h
+++ include/hermes/record_set.h
@@ -1,5 +1,21 @@
 /*
  * Copyright (c) 2016 Cossack Labs Limited
+ *
+ * This file is part of Hermes.
+ *
+ * Hermes is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Hermes is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Hermes.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 #ifndef MIDHERMES_RECORD_SET_H_
@@ -23,24 +39,6 @@ int record_set_client_get_private_block(record_set_client_t* rsc, const char* us
 int record_set_client_rm_public_block(record_set_client_t* rsc, const char* user_id, const char* block_id, const char* doc_id, const uint8_t* mac, const size_t mac_length);
 int record_set_client_rm_private_block(record_set_client_t* rsc, const char* user_id, const char* block_id, const char* doc_id, const uint8_t* mac, const size_t mac_length);
 
-/*int record_set_client_add_doc(record_set_client_t* rsc, const char* data, record_set_client_get_id_handler_t handler, void* ctx);
-int record_set_client_add_public_block(record_set_client_t* rsc, const char* doc_id, const uint8_t* data, const size_t data_length, record_set_client_get_id_handler_t handler, void* ctx);
-int record_set_client_add_private_block(record_set_client_t* rsc, const char* doc_id, const uint8_t* data, const size_t data_length, record_set_client_get_id_handler_t handler, void* ctx);
-
-int record_set_client_get_doc(record_set_client_t* rsc, const char* id, record_set_client_get_doc_handler_t handler, void* ctx);
-int record_set_client_get_doc_data(record_set_client_t* rsc, const char* id, record_set_client_get_doc_handler_t handler, void* ctx);
-int record_set_client_get_doc_breaf(record_set_client_t* rsc, const char* id, record_set_client_get_doc_handler_t handler, void* ctx);
-int record_set_client_get_public_block(record_set_client_t* rsc, const char* id, record_set_client_get_block_handler_t handler, void* ctx);
-int record_set_client_get_private_block(record_set_client_t* rsc, const char* id, record_set_client_get_block_handler_t handler, void* ctx);
-
-int record_set_client_update_doc(record_set_client_t* rsc, const char* id, const uint8_t* ut, const char* old_data, const char* data, record_set_client_get_res_handler_t handler, void* ctx);
-int record_set_client_update_public_block(record_set_client_t* rsc, const char* id, const uint8_t* ut, const uint8_t* old_data, const size_t old_data_len, const uint8_t* data, const size_t data_len, record_set_client_get_res_handler_t handler, void* ctx);
-int record_set_client_update_private_block(record_set_client_t* rsc, const char* id, const uint8_t* ut, const uint8_t* old_data, const size_t old_data_len, const uint8_t* data, const size_t data_len, record_set_client_get_res_handler_t handler, void* ctx);
-
-int record_set_client_delete_doc(record_set_client_t* rsc, const char* id, const uint8_t* ut, const size_t ut_len, record_set_client_get_res_handler_t handler, void* ctx);
-int record_set_client_delete_public_block(record_set_client_t* rsc, const char* id, const uint8_t* ut, const size_t ut_len, record_set_client_get_res_handler_t handler, void* ctx);
-int record_set_client_delete_private_block(record_set_client_t* rsc, const char* id, const uint8_t* ut, const size_t ut_len, record_set_client_get_res_handler_t handler, void* ctx);
-*/
 record_set_client_t* record_set_client_create(const config_t* config);
 int record_set_client_destroy(record_set_client_t* ctx);
 
