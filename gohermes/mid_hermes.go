227a2b7ff40ae983007af468bc76ec6798eaca23
andrey <andrey@cossacklabs.com>
2017-07-20 10:10:36 +0300

    add transport bindings for go

--- gohermes/mid_hermes.go
+++ gohermes/mid_hermes.go
@@ -22,6 +22,7 @@ package gohermes
 
 /*
 #include <hermes/rpc/transport.h>
+#include <hermes/mid_hermes/mid_hermes.h>
 #include <stdint.h>
 #include <assert.h>
 
@@ -40,7 +41,7 @@ uint32_t transport_recv(void* transport, uint8_t* buf, const size_t buf_length){
    return go_transport_recv(transport, buf, buf_length);
 }
 
-transport_t* create(void* transport){
+hm_rpc_transport_t* create(void* transport){
   hm_rpc_transport_t* result=calloc(1, sizeof(hm_rpc_transport_t));
   if(!result){
     return NULL;
@@ -98,3 +99,49 @@ func go_transport_recv(transport unsafe.Pointer, buf unsafe.Pointer, buf_length 
 	return 0;	
 }
 
+type MidHermes struct{
+	C.hm_rpc_transport_t* credential_store_transport;
+	C.hm_rpc_transport_t* data_store_transport;
+	C.hm_rpc_transport_t* key_store_transport;
+	C.mid_hermes_t* mid_hermes;
+}
+
+func MidHermes_finalize(mid_hermes *MidHermes){
+	mid_hermes.Close()
+}
+
+func New(id []byte, private_key []byte, credential_store_transport *transport_t, data_store_transport *transport_t, key_store_transport *transport_t) (*MidHermes, error){
+	mh := &MidHermes{
+		credential_store_transport: C.transport_create(unsafe.Pointer(credential_store_transport)),
+		data_store_transport: C.transport_create(unsafe.Pointer(data_store_transport)),
+		key_store_transport: C.transport_create(unsafe.Pointer(key_store_transport))}
+	mh.mid_hermes = C.mid_hermes_create(
+		unsafe.Pointer(&id[0]), C.size_t(len(id)),
+		unsafe.Pointer(&private_key[0]), C.size_t(len(private_key)),
+		mh.credential_store_transport,
+		mh.data_store_transport,
+		mh.key_store_transport)
+	if nil == mh.mid_hermes {
+		return nil, errors.New("MidHermes object creation error")
+	}
+	runtime.SetFinalizer(mh, MidHermes_finalize)
+	return mh, nil
+}
+
+func (mh *MidHermes) Close() error {
+	C.mid_hermes_destroy(&(mh.mid_hermes))
+	C.transport_destroy(&(mh.credential_store_transport))
+	C.transport_destroy(&(mh.data_store_transport))
+	C.transport_destroy(&(mh.key_store_transport))
+}
+
+func (mh *MidHermes)AddBlock(id []byte, data []byte, meta []byte) error{
+	if 0 != mid_hermes_create_block(
+		mh.mid_hermes,
+		unsafe.Pointer(&id[0]), C.size_t(len(id)),
+		unsafe.Pointer(&data[0]), C.size_t(len(data)),
+		unsafe.Pointer(&meta[0]), C.size_t(len(meta))){
+			return errors.New("MidHermes object creation error")
+		}
+	return nil
+}
