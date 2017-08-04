feb65afff6ee0f76edeecf291543af5022e523d1
andrey <andrey@cossacklabs.com>
2017-08-04 16:16:38 +0300

    correct license in .c && .h

--- docs/examples/c/mid_hermes/common/transport.c
+++ docs/examples/c/mid_hermes/common/transport.c
@@ -1,22 +1,24 @@
 /*
- * Copyright (c) 2017 Cossack Labs Limited
- *
- * This file is part of Hermes.
- *
- * Hermes is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Hermes is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with Hermes.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
+* Copyright (c) 2017 Cossack Labs Limited
+*
+* This file is a part of Hermes-core.
+*
+* Hermes-core is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Hermes-core is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with Hermes-core.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+
 
 #include "transport.h"
 
