7bcb923676875a720d6077b41cfbb5f6a47d7376
andrey <andrey@cossacklabs.com>
2017-08-04 16:21:01 +0300

    correct license in .mk

--- src/rpc/rpc.mk
+++ src/rpc/rpc.mk
@@ -1,20 +1,20 @@
 #
-# Copyright (c) 2016 Cossack Labs Limited
+# Copyright (c) 2017 Cossack Labs Limited
 #
-# This file is part of Hermes.
+# This file is a part of Hermes-core.
 #
-# Hermes is free software: you can redistribute it and/or modify
+# Hermes-core is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Hermes is distributed in the hope that it will be useful,
+# Hermes-core is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with Hermes.  If not, see <http://www.gnu.org/licenses/>.
+# along with Hermes-core.  If not, see <http://www.gnu.org/licenses/>.
 #
 #
 
