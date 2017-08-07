87e2cc42e1728597ecc3aca4d54e07c46ec73f8b
andrey <andrey@cossacklabs.com>
2017-08-07 13:48:55 +0300

    Corect for #44

--- docs/examples/c/mid_hermes_low_level/test-all.sh
+++ docs/examples/c/mid_hermes_low_level/test-all.sh
@@ -9,18 +9,18 @@ mkdir -p ./db/data_store
 #generate 3 users
 for i in {1..3}
 do
-    ../key_gen/key_pair_gen user$i.priv ./db/credential_store/`echo -ne 'user'$i'\0' | base64`
+    ../key_gen/key_pair_gen user$i.priv ./db/credential_store/`echo -ne 'user'$i | base64`
     for j in {1..3}
     do
 	#generate random block
 	dd bs=1024 count=1024 </dev/urandom >user$i-file$j.data
 	#add block to hermes
-	./hermes_client_ll ba user$i `cat user$i.priv | base64` user$i-file$j.data "$j file of user$i"
+	./hermes_client_ll add_block user$i `cat user$i.priv | base64` user$i-file$j.data "$j file of user$i"
 	if [ $? -ne 0 ]; then
 	    echo user$i-file$j.data adding error
 	fi
 	#read block from hermes
-	./hermes_client_ll br user$i `cat user$i.priv | base64` user$i-file$j.data
+	./hermes_client_ll read_block user$i `cat user$i.priv | base64` user$i-file$j.data
 	if [ $? -ne 0 ]; then
 	    echo user$i-file$j.data getting error
 	fi
