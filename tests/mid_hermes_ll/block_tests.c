7b525bae9b60e9b525e743f7f8deaf65a697f73c
Vixentael <vixentael@gmail.com>
2017-10-24 18:02:13 +0300

    add tests: buffer/user equality check, cnst_memcmp check

--- tests/mid_hermes_ll/block_tests.c
+++ tests/mid_hermes_ll/block_tests.c
@@ -25,6 +25,11 @@
 #include <common/common.h>
 #include "block_tests.h"
 
+int mid_hermes_ll_block_buffer_equality_tests ();
+int mid_hermes_ll_block_user_equality_tests ();
+int mid_hermes_ll_block_cst_time_memcmp_tests ();
+
+
 int mid_hermes_ll_block_tests(){
     // generate test keypair
     uint8_t* private_key=NULL;
@@ -66,7 +71,6 @@ int mid_hermes_ll_block_tests(){
 
     mid_hermes_ll_token_t* readToken = mid_hermes_ll_token_generate(mid_hermes_ll_user_copy(user));
     mid_hermes_ll_token_t* writeToken = mid_hermes_ll_token_generate(mid_hermes_ll_user_copy(user));
-
 
     // test that block not created without write token
     mid_hermes_ll_block_t *block = mid_hermes_ll_block_create_new(user, blockIdBuffer, blockDataBuffer, blockMetaBuffer, readToken, NULL);
@@ -115,5 +119,123 @@ int mid_hermes_ll_block_tests(){
     testsuite_fail_if(!block->block, "block->block != NULL");
     testsuite_fail_if(!block->user, "block->user != NULL");
 
+    // test equality
+    int equalityTestResult = mid_hermes_ll_block_buffer_equality_tests();
+    testsuite_fail_if(equalityTestResult != TEST_SUCCESS, "mid_hermes_ll_block_buffer_equality_tests check");
+
+    equalityTestResult = mid_hermes_ll_block_user_equality_tests();
+    testsuite_fail_if(equalityTestResult != TEST_SUCCESS, "mid_hermes_ll_block_user_equality_tests check");
+
+    equalityTestResult = mid_hermes_ll_block_cst_time_memcmp_tests();
+    testsuite_fail_if(equalityTestResult != TEST_SUCCESS, "mid_hermes_ll_block_cst_time_memcmp_tests check");
+
     return TEST_SUCCESS;
+}
+
+int mid_hermes_ll_block_buffer_equality_tests () {
+    uint8_t *userId1 = (uint8_t*)"user id1";
+    mid_hermes_ll_buffer_t* userBuffer1 = mid_hermes_ll_buffer_create(userId1, 8);
+
+    uint8_t *userId1Twin = (uint8_t*)"user id1";
+    mid_hermes_ll_buffer_t* userBuffer1Twin = mid_hermes_ll_buffer_create(userId1, 8);
+
+    uint8_t *userId2 = (uint8_t*)"user id2";
+    mid_hermes_ll_buffer_t* userBuffer2 = mid_hermes_ll_buffer_create(userId2, 8);
+
+    uint8_t *userId3 = (uint8_t*)"user id3";
+    mid_hermes_ll_buffer_t* userBufferWrongLength = mid_hermes_ll_buffer_create(userId3, 1);
+
+    mid_hermes_ll_buffer_t* userBufferWrongData = mid_hermes_ll_buffer_create(NULL, 1);
+    mid_hermes_ll_buffer_t* userBufferWrongData2 = mid_hermes_ll_buffer_create(NULL, 56);
+
+    bool isEqual = mid_hermes_ll_buffer_is_equal(userBuffer1, userBuffer1);
+    testsuite_fail_if(!isEqual, "buffer_is_equal should be equal if block is the same");
+
+    isEqual = mid_hermes_ll_buffer_is_equal(userBuffer1, userBuffer1Twin);
+    testsuite_fail_if(!isEqual, "buffer_is_equal should be equal if blocks have same content");
+
+    isEqual = mid_hermes_ll_buffer_is_equal(userBuffer1, userBuffer2);
+    testsuite_fail_if(isEqual, "buffer_is_equal should NOT be equal if blocks are different");
+
+    isEqual = mid_hermes_ll_buffer_is_equal(userBuffer2, userBufferWrongLength);
+    testsuite_fail_if(isEqual, "buffer_is_equal should NOT be equal if blocks have different lenght");
+
+    isEqual = mid_hermes_ll_buffer_is_equal(userBuffer1, userBufferWrongData);
+    testsuite_fail_if(isEqual, "buffer_is_equal should NOT be equal if block has NULL data");
+
+    isEqual = mid_hermes_ll_buffer_is_equal(userBufferWrongData2, userBufferWrongData);
+    testsuite_fail_if(isEqual, "buffer_is_equal should NOT be equal if both blocks have NULL data");
+
+    return TEST_SUCCESS; 
+}
+
+int mid_hermes_ll_block_user_equality_tests () {
+
+    int KEY_LENGTH = 256;
+
+    size_t private_key_length1=KEY_LENGTH;
+    size_t public_key_length1=KEY_LENGTH;
+
+    uint8_t* private_key1 = malloc(private_key_length1);
+    uint8_t* public_key1 = malloc(public_key_length1);
+    if(themis_gen_ec_key_pair(private_key1, &private_key_length1, public_key1, &public_key_length1)!=THEMIS_SUCCESS){
+        testsuite_fail_if(true, "generate key pair");
+        return TEST_FAIL;
+    }
+
+    size_t private_key_length2=KEY_LENGTH;
+    size_t public_key_length2=KEY_LENGTH;
+
+    uint8_t* private_key2 = malloc(private_key_length2);
+    uint8_t* public_key2 = malloc(public_key_length2);
+    if(themis_gen_ec_key_pair(private_key2, &private_key_length2, public_key2, &public_key_length2)!=THEMIS_SUCCESS){
+        testsuite_fail_if(true, "generate key pair");
+        return TEST_FAIL;
+    }
+
+    uint8_t *userId1 = (uint8_t*)"user id1";
+    mid_hermes_ll_buffer_t* userBuffer1 = mid_hermes_ll_buffer_create(userId1, 8);
+    mid_hermes_ll_buffer_t* privateKeyBuffer1 = mid_hermes_ll_buffer_create(private_key1, private_key_length1);
+    mid_hermes_ll_buffer_t* publicKeyBuffer1 = mid_hermes_ll_buffer_create(public_key1, public_key_length1);
+    mid_hermes_ll_user_t* user1 = mid_hermes_ll_local_user_create(userBuffer1, privateKeyBuffer1, publicKeyBuffer1);
+
+
+    uint8_t *userId2 = (uint8_t*)"user id2";
+    mid_hermes_ll_buffer_t* userBuffer2 = mid_hermes_ll_buffer_create(userId2, 8);
+    mid_hermes_ll_buffer_t* privateKeyBuffer2 = mid_hermes_ll_buffer_create(private_key2, private_key_length2);
+    mid_hermes_ll_buffer_t* publicKeyBuffer2 = mid_hermes_ll_buffer_create(public_key2, public_key_length2);
+    mid_hermes_ll_user_t* user2 = mid_hermes_ll_local_user_create(userBuffer2, privateKeyBuffer2, publicKeyBuffer2);
+
+    bool isEqual = mid_hermes_ll_user_is_equal(user1, user1);
+    testsuite_fail_if(!isEqual, "buffer_is_equal should be equal if users are the same");
+
+    isEqual = mid_hermes_ll_user_is_equal(user1, user2);
+    testsuite_fail_if(isEqual, "buffer_is_equal should NOT be equal if users are different");
+
+    return TEST_SUCCESS; 
+}
+
+int mid_hermes_ll_block_cst_time_memcmp_tests () {
+
+    uint8_t data1[3] = {0xAB, 0xAC, 0x12};
+    uint8_t data2[3] = {0xAB, 0xAC, 0x12};
+    uint8_t data3[3] = {0xAA, 0xAA, 0x22};
+    uint8_t data4[4] = {0xAB, 0xAC, 0x12, 0x12};
+
+    int cmp_result = cst_time_memcmp(data1, data1, sizeof(data1) / sizeof(uint8_t));
+    testsuite_fail_if(cmp_result != 0, "cst_time_memcmp should be equal if data is the same");
+
+    cmp_result = cst_time_memcmp(data1, data2, sizeof(data1) / sizeof(uint8_t));
+    testsuite_fail_if(cmp_result != 0, "cst_time_memcmp should be equal if data has same content");
+
+    cmp_result = cst_time_memcmp(data1, data3, sizeof(data1) / sizeof(uint8_t));
+    testsuite_fail_if(cmp_result == 0, "cst_time_memcmp should NOT be equal if data is different");
+
+    cmp_result = cst_time_memcmp(data1, data4, sizeof(data1) / sizeof(uint8_t));
+    testsuite_fail_if(cmp_result != 0, "cst_time_memcmp might be equal if data is different, size is different, comparasion is using shorted size");
+
+    cmp_result = cst_time_memcmp(data1, data4, sizeof(data4) / sizeof(uint8_t));
+    testsuite_fail_if(cmp_result == 0, "cst_time_memcmp should NOT be equal if data is different, size is different, comparasion is using longer size");
+
+    return TEST_SUCCESS; 
 }
