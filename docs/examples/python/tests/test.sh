46631b338ff9e973b1f2340d6c94a7e3d19dfead
Dmitriy Korneev <lagovas.lagovas@gmail.com>
2017-11-10 16:24:09 +0200

    add license headers

--- docs/examples/python/tests/test.sh
+++ docs/examples/python/tests/test.sh
@@ -1,4 +1,24 @@
 #!/usr/bin/env bash
+#
+# Copyright (c) 2017 Cossack Labs Limited
+#
+# This file is a part of Hermes-core.
+#
+# Hermes-core is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Hermes-core is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Hermes-core.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
 TEST_FILE=/tmp/testfile
 echo "some content" > $TEST_FILE
 USER_ID=user1
