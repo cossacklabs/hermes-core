dfeef95cfc6cd445b6dc98635b171c126451e811
Lagovas <lagovas.lagovas@gmail.com>
2017-11-14 16:15:31 +0200

    test examples (#92)
    Update circle.yml and tests

--- docs/examples/python/tests/test.sh
+++ docs/examples/python/tests/test.sh
@@ -22,10 +22,14 @@
 TEST_FILE=/tmp/testfile
 echo "some content" > $TEST_FILE
 USER_ID=user1
-PRIVATE_KEY1=/path/to/private/key1
+PRIVATE_KEY1=/tmp/user1.priv
+PRIVATE_KEY_BASE64=UkVDMgAAAC0Tj5tGAPfpgfYMBACxX6onvlWvcc2Gb9ZylBlJdjebTpV3OCIx
+echo -n $PRIVATE_KEY_BASE64 | base64 -d > $PRIVATE_KEY1
 
 USER_ID2=user2
-PRIVATE_KEY2=/path/to/private/key2
+PRIVATE_KEY2=/tmp/user2.priv
+PRIVATE_KEY2_BASE64=UkVDMgAAAC00lzw7ABmvKHvjOqWW8i+dxwHTU8RzuaATkZNBcLmCm8TBxRn2
+echo -n $PRIVATE_KEY2_BASE64 | base64 -d > $PRIVATE_KEY2
 CONFIG=docs/examples/python/config.json
 BIN=docs/examples/python/hermes_client.py
 
@@ -35,14 +39,14 @@ FAIL=1
 # add block
 echo "add block"
 python $BIN --id $USER_ID --private_key $PRIVATE_KEY1 --doc $TEST_FILE --meta "metadata" --config=$CONFIG --add
-if [ $? -eq $FAIL ]; then
+if [ ! $? -eq $SUCCESS ]; then
     exit 1
 fi
 
 # read block
 echo "read block"
 python $BIN --id $USER_ID --private_key $PRIVATE_KEY1 --doc $TEST_FILE --config=$CONFIG --read
-if [ $? -eq $FAIL ]; then
+if [ ! $? -eq $SUCCESS ]; then
     exit 1
 fi
 
@@ -50,7 +54,7 @@ fi
 echo "update block"
 echo "some new content" > $TEST_FILE
 python $BIN --id $USER_ID --private_key $PRIVATE_KEY1 --doc $TEST_FILE --meta "new metadata" --config=$CONFIG --update
-if [ $? -eq $FAIL ]; then
+if [ ! $? -eq $SUCCESS ]; then
     exit 1
 fi
 
@@ -65,14 +69,14 @@ fi
 # grant access
 echo "grant read access"
 python $BIN --id $USER_ID --private_key $PRIVATE_KEY1 --doc $TEST_FILE --config=$CONFIG --grant_read --for_user=$USER_ID2
-if [ $? -eq $FAIL ]; then
+if [ ! $? -eq $SUCCESS ]; then
     exit 1
 fi
 
 # read by user 2
 echo "read by user 2"
 python $BIN --id $USER_ID2 --private_key $PRIVATE_KEY2 --doc $TEST_FILE --config=$CONFIG --read
-if [ $? -eq $FAIL ]; then
+if [ ! $? -eq $SUCCESS ]; then
     exit 1
 fi
 
@@ -88,7 +92,7 @@ fi
 echo "grant update"
 # grant update to user 2
 python $BIN --id $USER_ID --private_key $PRIVATE_KEY1 --doc $TEST_FILE --config=$CONFIG --grant_update --for_user=$USER_ID2
-if [ $? -eq $FAIL ]; then
+if [ ! $? -eq $SUCCESS ]; then
     exit 1
 fi
 
@@ -96,7 +100,7 @@ fi
 echo "update block by user 2"
 echo "some new content by user2" > $TEST_FILE
 python $BIN --id $USER_ID2 --private_key $PRIVATE_KEY2 --doc $TEST_FILE --meta "new metadata 2" --config=$CONFIG --update
-if [ $? -eq $FAIL ]; then
+if [ ! $? -eq $SUCCESS ]; then
     exit 1
 fi
 
@@ -104,7 +108,7 @@ fi
 # revoke update from user 2
 echo "revoke update"
 python $BIN --id $USER_ID --private_key $PRIVATE_KEY1 --doc $TEST_FILE --config=$CONFIG --revoke_update --for_user=$USER_ID2
-if [ $? -eq $FAIL ]; then
+if [ ! $? -eq $SUCCESS ]; then
     exit 1
 fi
 
@@ -119,7 +123,7 @@ fi
 # revoke read from user 2
 echo "revoke read"
 python $BIN --id $USER_ID --private_key $PRIVATE_KEY1 --doc $TEST_FILE --config=$CONFIG --revoke_read --for_user=$USER_ID2
-if [ $? -eq $FAIL ]; then
+if [ ! $? -eq $SUCCESS ]; then
     exit 1
 fi
 
@@ -133,6 +137,6 @@ fi
 # delete block
 echo "delete block"
 python $BIN --id $USER_ID --private_key $PRIVATE_KEY1 --doc $TEST_FILE --config=$CONFIG --delete
-if [ $? -eq $FAIL ]; then
+if [ ! $? -eq $SUCCESS ]; then
     exit 1
 fi
