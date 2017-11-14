2088ca8459769d35bf3d4cac43bb719c52b5e34f
Dmitriy Korneev <lagovas.lagovas@gmail.com>
2017-11-14 15:12:46 +0200

    use exit code in tests

---
+++ tests/scripts/test_services.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+
+# testing with running services:
+# 1. delete all files in key_store/data_store from previous tests
+# 2. start services and save their PID in env variable to kill them at finish
+# 3. run test
+# 4. save test exit code in variable to not break test running and halting services correctly
+# 5. stop services
+# 6. return test exit code
+
+mkdir -p db/key_store db/data_store db/credential_store
+rm -rf db/key_store/*;
+rm -rf db/data_store/*;
+
+./docs/examples/c/mid_hermes/credential_store_service/cs &
+CS_PID=$!;
+
+./docs/examples/c/mid_hermes/key_store_service/ks &
+KS_PID=$!;
+
+./docs/examples/c/mid_hermes/data_store_service/ds &
+DS_PID=$!;
+
+# run script from arg1
+eval $@;
+RESULT=$?;
+
+kill -9 $CS_PID $KS_PID $DS_PID;
+
+exit $RESULT
